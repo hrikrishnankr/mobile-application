711748aff0a6eeff350718057138fd9891ad1eea
Hari Krishnan <hari@qburst.com>
2015-12-28 14:44:09 +0530

    second commit

--- JS/index.js
+++ JS/index.js
@@ -2,27 +2,27 @@ toggle=0;
 window.onload=function(){
     toggle=0;
     document.getElementById("xyz").style.position="fixed";
-    document.getElementById("xyz").style.right="-400px";
+    document.getElementById("xyz").style.right="-600px";
 };
-      function show(){
-         if(parseInt(document.getElementById("xyz").style.right)<=-5){
-            document.getElementById("xyz").style.right=parseInt(document.getElementById("xyz").style.right)+5+"px";
-            setTimeout(show,9);
-         }
-      }
-      function hide () {
-         if(parseInt(document.getElementById("xyz").style.right)>=-395){
-            document.getElementById("xyz").style.right=parseInt(document.getElementById("xyz").style.right)-5+"px";
-            setTimeout(hide,9);
-         }
-      }
-      function menuToggle(){
-         if(toggle==0){
-            show();
-            toggle=1;
-         }
-         else if(toggle==1){
-            hide();
-            toggle=0;   
-         }
-      }
+function show(){
+   if(parseInt(document.getElementById("xyz").style.right)<=-6){
+      document.getElementById("xyz").style.right=parseInt(document.getElementById("xyz").style.right)+5+"px";
+      setTimeout(show,5);
+   }
+}
+function hide () {
+   if(parseInt(document.getElementById("xyz").style.right)>=-595){
+      document.getElementById("xyz").style.right=parseInt(document.getElementById("xyz").style.right)-5+"px";
+      setTimeout(hide,5);
+   }
+}
+function menuToggle(){
+   if(toggle==0){
+      show();
+      toggle=1;
+   }
+   else if(toggle==1){
+      hide();
+      toggle=0;   
+   }
+}
