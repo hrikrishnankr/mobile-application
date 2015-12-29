ae39347807fa9ffdeca83cce7c5a792d20f92c63
Hari Krishnan <hari@qburst.com>
2015-12-29 12:37:32 +0530

    Adding pages 7

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,18 @@
-toggle=0;
+var toggle=0;
 window.onload=function(){
     toggle=0;
     document.getElementById("xyz").style.position="fixed";
     document.getElementById("xyz").style.right="-50%";
 };
 function show(){
+   document.getElementById("content").style.color="rgba(0,0,0,0.5)";
    if(parseInt(document.getElementById("xyz").style.right)<=-2){
       document.getElementById("xyz").style.right=parseInt(document.getElementById("xyz").style.right)+1+"%";
       setTimeout(show,5);
    }
 }
 function hide () {
+	document.getElementById("content").style.color="black";
    if(parseInt(document.getElementById("xyz").style.right)>=-50){
       document.getElementById("xyz").style.right=parseInt(document.getElementById("xyz").style.right)-1+"%";
       setTimeout(hide,5);
